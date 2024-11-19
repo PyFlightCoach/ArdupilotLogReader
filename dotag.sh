93e8d23a8ef13c2d61092e5e2469cf221cdc4753
Thomas David <thomasdavid0@gmail.com>
2024-11-19 16:24:53 +0000

    Update Version

---
+++ dotag.sh
@@ -0,0 +1,13 @@
+
+
+VERSION=$(git tag --sort=committerdate | grep -E '[0-9]' | tail -1 | cut -b 2-7)
+
+NEXTVERSION=$(echo ${VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+echo "Last version: ${VERSION}"
+echo "Next version: ${NEXTVERSION}"
+
+git push --delete origin $NEXTVERSION
+git add --all
+git commit -m "Update Version"
+git tag -a v${NEXTVERSION} -m "Update Version"
+git push origin v${NEXTVERSION}
